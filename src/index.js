e3d99e32824b286f0d1fb51f006e2da0f8007d97
luzciel <luzcielmontesinos@gmail.com>
2020-10-31 19:54:37 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import validator from './validator.js';
 
-console.log(validator);
+console.log("AAAAAAAAAAAAAa")
+
+var modal = document.getElementById("miModal");
+
+//funcion que establece el mensaje que aparecera en caso de que la tarjeta sea valida o no
+function botonPago() {
+  var creditCardNumber = document.getElementById("numeroTDC").value;
+  if (creditCardNumber.length === 16 && validator.isValid(creditCardNumber) === true) {
+    document.getElementById("validacion").innerHTML ='<a href="#">Felicidades ya eres parte de la familia Bardi.</a>';
+    document.getElementById("ocultarNum").innerHTML ='<a href="#">Tus suscripción esta afiliada a la tarjeta ' + validator.maskify(creditCardNumber) +'.</a>';
+  } else {
+    document.getElementById("validacion").innerHTML ='<a href="#">Error: </a>'
+    document.getElementById("ocultarNum").innerHTML ='<a href="#">' + validator.maskify(creditCardNumber) + ' numero de tarjeta invalido.</a>';
+  }
+  modal.style.display = "block";
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0]; 
+
+// Cuando se haga click <span> (x), cierra el modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+//Al hacer click en el boton aceptar del modal aplica la funcion volverPag()
+document.getElementById("volver").onclick = function() {volverPag()};
+
+// funcion que designa que hara el boton ACEPTAR. Si la la tarjeta es validad (true) vuelve a la pagina anterior (back)
+// y si es false desaparece el modal (style= "none")
+function volverPag() {
+  var creditCardNumber = document.getElementById("numeroTDC").value;
+  if (creditCardNumber.length === 16 && validator.isValid(creditCardNumber) === true) {
+    window.history.back()
+  } else {
+    modal.style.display = "none";
+  }  
+}
+
+// Cuando se haga clicks fuera del modal, este desaparece 
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+window.botonPago = botonPago;
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,50 @@
-const validator = {
-  // ...
-};
+const validator = { isValid, maskify}
+
+
+function isValid(numeroTarjeta) {
+      var sum = 0;
+        for (var i = 0; i < numeroTarjeta.length; i++) {
+            //console.log(numeroTarjeta[i]);
+        // primero convierte el string en un numero y devuelveme los caracteres (iniciando en la posicion que tenga i  y tomando como longitud 1 solo caracter)
+            //4137894711755904
+            // 5166187454592415
+            //6011895316376257
+            //3589659809783950
+            //4812127914185757
+            //4738362374825599
+            //3625102593804 ejemplo error
+            //4083952015263
+            var digito = parseInt(numeroTarjeta.substr(i, 1));  // substr=Localización en la cual se empiezan a extraer caracteres
+            if (i % 2 !== 0) {
+            //console.log("pos", i);
+            //console.log("val", numeroTarjeta[i]);
+                digito *= 2;
+                //console.log("multiplicar", digito);
+                if (digito > 9) {
+                    digito = 1 + (digito % 10);
+                    //console.log("mayor10", digito);
+                }
+            }
+            sum += digito;
+            //console.log("sumaTotal", sum)
+        }
+        console.log('SUM es:',sum)
+        return (sum % 10) == 0;    
+    }   
+
+
+function maskify(numeroTarjeta) {
+    //var numeroTarjeta = document.getElementById("numeroTDC").value;
+        var imprimir = "";
+        if( numeroTarjeta.length <= 4){
+          imprimir= numeroTarjeta;
+          }
+          else {
+            //"".padEnd(12, "*") + hello.substring (12, 16)
+            imprimir = "".padEnd(numeroTarjeta.length -4, "#") + numeroTarjeta.substring(numeroTarjeta.length -4, numeroTarjeta.length)
+            }
+        console.log(imprimir);
+        return imprimir 
+        }
 
 export default validator;
